85c4a319f82bd186ad7939c7d41c8c792d481724
Ankit Prasad <mail@ankitprasad.com>
2015-05-31 11:08:54 -0700

    Initial Commit

---
+++ tipster2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipster2
+//
+//  Created by Ankit Prasad on 5/31/15.
+//  Copyright (c) 2015 Ankit Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
